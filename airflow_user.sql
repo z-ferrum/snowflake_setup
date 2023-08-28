44403f1d6d11e9cc3c7b9463a20d05eff69a1535
iurii zhelezniakov <yuriy.zheleznyakov@gmail.com>
2023-08-28 10:17:26 +0200

    airflow user

--- airflow_user.sql
+++ airflow_user.sql
@@ -3,3 +3,30 @@
 Describes setting up airflow role for airflow itself and for airflow users
 
 */
+
+-- create the airflow role
+create role airflow_role;
+
+-- base usage permissions on the database, schema and warehouse
+grant usage on database analytics to role airflow_role;
+grant usage on schema analytics.raw to role airflow_role;
+grant usage on warehouse airflow_wh to role airflow_role;
+
+-- permissions to the stage
+grant usage, read, write on stage analytics.raw.raw_prod to role airflow_role;
+
+-- grant permissions to insert into, update, delete, and select from the tables
+-- update - under question, for larger data you may want to append only
+grant select, insert, update, delete on all tables in schema analytics.raw to role airflow_role;
+grant select, insert, update, delete on future tables in schema analytics.raw to role airflow_role;
+
+-- Create a new user for Airflow and give him the role
+create user airflow_user
+email = 'xxx@gmail.com'
+password = 'xxx'
+default_role = airflow_role -- ex. the role above
+default_warehouse = airflow_wh
+must_change_password = true;
+
+-- grant the airflow_role to a specific user or another role
+grant role airflow_role to user airflow_user;
